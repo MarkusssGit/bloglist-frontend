a6283082e1cff2c7375821337657c70814c51fdc
MarkusssGit <106807553+MarkusssGit@users.noreply.github.com>
2023-08-11 16:42:59 +0300

    5.2: blogilistan frontend, step2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +17,96 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+    console.log('logging in with', username, password)
+  }
+
+  const handleLogout = async (event) => {
+    loginService.logout('loggedBlogappUser')
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <h2>Login info</h2>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  const showBlogs = () => (
     <div>
       <h2>blogs</h2>
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+    
+  )
+
+  /*const blogForm = () => (
+    <form onSubmit={addblog}>
+      <input
+        value={newblog}
+        onChange={handleblogChange}
+      />
+      <button type="submit">save</button>
+    </form>  
+  )*/
+
+  return (
+    <div>
+      <Notification message={errorMessage} />
+      {!user && loginForm()}
+      {user && <div>
+       <p>{user.name} logged in</p> <form onSubmit={handleLogout}><button type="submit">log out</button></form>
+      </div>
+      }
+      {user && showBlogs()}
     </div>
   )
 }
