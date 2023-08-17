6fe4cf22052f255b8b2694fd2bdef9b1ded09142
MarkusssGit <106807553+MarkusssGit@users.noreply.github.com>
2023-08-17 13:33:29 +0300

    5.10: blogilistan frontend, step10

--- src/App.js
+++ src/App.js
@@ -83,19 +83,21 @@ const App = () => {
     )
   }
 
-  const showBlogs = () => (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-      <Blog 
-        key={blog.id} 
-        blog={blog}
-        handlelike= {handlelike}
-      />
-      )}
-    </div>
-    
-  )
+  const showBlogs = () => {
+    blogs.sort((a, b) => b.likes - a.likes)
+    return (
+      <div>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+        <Blog 
+          key={blog.id} 
+          blog={blog}
+          handlelike= {handlelike}
+        />
+        )}
+      </div>
+    )
+  }
 
   const blogForm = () => (
     <div>
